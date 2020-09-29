eabf885142fabfaf9acb5a99d9d2112d7c725a22
Camilla Minutilli <camilla.minutilli@gmail.com>
2020-09-29 21:35:09 +0200

    Added translastions

---
+++ src/settings.ts
@@ -0,0 +1,47 @@
+export const languages = ['it', 'en'];
+export const translations = {
+    title: {
+        it: 'Le notizie in un solo posto',
+        en: 'News in one place'
+    },
+    ctaEditPage: {
+        it: 'Scegli le tue fonti',
+        en: 'Choose your sources'
+    },
+    editLink: {
+        it: 'Modifica',
+        en: 'Edit'
+    },
+    previewLink: {
+        it: 'Anteprima',
+        en: 'Preview'
+    },
+    changeLanguage: {
+        it: 'Cambia lingua',
+        en: 'Change language'
+    },
+    select: {
+        it: 'Seleziona',
+        en: 'Select'
+    },
+    selectAll: {
+        it: 'Seleziona tutti',
+        en: 'Select all'
+    },
+    add: {
+        it: 'Aggiungi',
+        en: 'Add'
+    },
+    remove: {
+        it: 'Rimuovi',
+        en: 'Remove'
+    },
+    order: {
+        it: 'Ordina la lista',
+        en: 'Order the list'
+    },
+    orderByDragging: {
+        it: "Trascina e muovi l'elemento per riordinare la lista",
+        en: 'Drag and move the item to re-order the list'
+    }
+};
